c771e5f92564048fbce18241300a07383af70195
Sparsh Gupta <sparshgupta.a8@gmail.com>
2017-10-07 17:44:17 +0530

    First calculator commit

---
+++ CalculatorUI.java
@@ -0,0 +1,13 @@
+package Calculator;
+
+import javax.swing.JFrame;
+import javax.swing.*;
+
+public class CalculatorUI extends JFrame {
+
+	JPanel[] row=new JPanel[5];
+	JButton[] display=new JButton[19];
+	String buttonString[]= {"7","8","9","+","C","4","5","6","-","?",
+			"1","2","3","*","+/-","0",".","/","="};
+	
+}
