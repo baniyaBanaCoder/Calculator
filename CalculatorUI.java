5ce34bae0c3a7bcf99ac5203ce3b88fccc72d019
Sparsh Gupta <sparshgupta.a8@gmail.com>
2017-10-08 17:08:28 +0530

    Second Commit even better

--- CalculatorUI.java
+++ CalculatorUI.java
@@ -1,13 +1,45 @@
 package Calculator;
 
-import javax.swing.JFrame;
+import java.awt.*;
+
 import javax.swing.*;
 
 public class CalculatorUI extends JFrame {
 
 	JPanel[] row=new JPanel[5];
-	JButton[] display=new JButton[19];
+	JButton[] button=new JButton[19];
 	String buttonString[]= {"7","8","9","+","C","4","5","6","-","?",
 			"1","2","3","*","+/-","0",".","/","="};
-	
+
+	JTextArea display=new JTextArea(2,50);
+	Font font=new Font("Chiller",Font.BOLD,20);
+
+	CalculatorUI() {
+		super("Calculator");
+		setDesign();
+		setSize(250,350);
+		setResizable(false);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		GridLayout grid=new GridLayout(5,5);
+		setLayout(grid);
+
+		FlowLayout f1=new FlowLayout(FlowLayout.CENTER);
+		FlowLayout f2=new FlowLayout(FlowLayout.CENTER,1,1);
+
+		for(int i=0;i<5;i++)
+			row[i]=new JPanel();
+		row[0].setLayout(f1);
+		for(int i=1;i<5;i++)
+			row[i].setLayout(f2);
+
+		for(int i=0;i<19;i++){
+			button[i]=new JButton();
+			button[i].setText(buttonString[i]);
+			button[i].setBackground(Color.GRAY);
+			button[i].setFont(font);
+		}
+
+
+	}
+
 }
